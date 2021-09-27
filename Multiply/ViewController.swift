778996eed021d06f9cc6e59df700b42f69a68d45
Makachai Jacobs <malachai.jacobs15@gmail.com>
2021-09-27 10:15:16 -0600

    MVP

--- Multiply/ViewController.swift
+++ Multiply/ViewController.swift
@@ -6,14 +6,51 @@
 //
 
 import UIKit
+@IBDesignable extension UIButton {
+//From stackoverflow
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
 
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+   
     }
-
+    
+    @IBAction func calculateButton(_ sender: Any)
+    {
+        
+        
+    }
+    
 
 }
 

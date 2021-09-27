08e7f4c678299a155bbc3967e8b1bc435f1ec252
Makachai Jacobs <malachai.jacobs15@gmail.com>
2021-09-27 10:50:18 -0600

    MVP variables

--- Multiply/ViewController.swift
+++ Multiply/ViewController.swift
@@ -37,20 +37,35 @@ import UIKit
         }
     }
 }
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    //MARK: MVP Variables
+    @IBOutlet weak var textField1: UITextField!
+    let Number1:String = ""
+    @IBOutlet weak var textField2: UITextField!
+    let Number2:String = ""
+    
+    @IBOutlet weak var label1: UILabel!
+    override func viewDidLoad()
+    
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
    
     }
     
+    //MARK: MVP Button
+    
     @IBAction func calculateButton(_ sender: Any)
     {
+        var Num1 = textField1.text ?? "0"
+         var intergerNumber1 = Int(Num1) ?? 0
         
-        
+        var Num2 = textField2.text ?? "0"
+        var intergerNumber2 = Int(Num2) ?? 0
     }
     
-
 }
+    
+
 
